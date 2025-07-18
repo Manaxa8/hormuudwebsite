2327093864a7bb528fc2b07f0e0edb9e287a0614
Abdirahman <abdirahmanaden3872@gmail.com>
2025-07-18 17:30:36 +0300

    Update hormuud.js

--- hormuud.js
+++ hormuud.js
@@ -1,19 +1,3 @@
-document.addEventListener("DOMContentLoaded",() =>{
-const PersonalToggle= document.getElementById("PersonalToggle")
-const Personalmenu= document.getElementById("Personalmenu")
 
-PersonalToggle.addEventListener("click",(e)=>{
-    e.preventDefault();
-    Personalmenu.classList.toggle("show");
-});
-
-document.addEventListener("click",(e) =>{
-if(!PersonalToggle.contains(e.target) && !
-Personalmenu.contains(e.target)){
-    Personalmenu.classList.remove("show");
-}
-
-});
-});
 
 
